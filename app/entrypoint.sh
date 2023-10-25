17b7aa21aea85b55df57069242413e7b467e8b2a
smellslikeml <trollofberkeley@gmail.com>
2023-10-24 21:47:09 -0700

    simplified scraper

---
+++ app/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+echo "Waiting for index to be available..."
+sleep 30
+
+# Now start your app
+python3 /app/app.py
--- scraper/main.py
+++ scraper/main.py
@@ -9,9 +9,12 @@ from sentence_transformers import (
     SentencesDataset,
     LoggingHandler,
     losses,
+    InputExample,
 )
 from torch.utils.data import DataLoader
 from elasticsearch import Elasticsearch
+from urllib.parse import urlparse
+
 
 
 def wait_for_elasticsearch(es_host):
@@ -27,19 +30,28 @@ def wait_for_elasticsearch(es_host):
 
 class TextSpider(scrapy.Spider):
     name = "text_spider"
+    items = []
     with open("config.json", "r") as f:
         config = json.load(f)
         start_urls = config.get("start_urls", [])
 
     custom_settings = {
-        "DEPTH_LIMIT": 3,  # Adjust as needed
+        "DEPTH_LIMIT": 1,  # Setting to 1 ensures only the start URLs are processed
         "DOWNLOAD_DELAY": 1,  # Adjust delay between requests
+        # Other custom settings to prevent blocks or restrictions
+        "USER_AGENT": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36",
+        "DOWNLOAD_TIMEOUT": 30,  # You can adjust this as per your needs
+        "RETRY_ENABLED": True,
+        "RETRY_TIMES": 3,  # Total number of retries for each request
+        "COOKIES_ENABLED": False,  # Disabling cookies as some sites might track based on cookies
+        # If the site uses AJAX, you might consider enabling the middlewares below:
+        # 'scrapy.downloadermiddlewares.ajaxcrawl.AjaxCrawlMiddleware': 400,
     }
 
     def parse(self, response):
-        yield {"url": response.url, "text": response.xpath("//text()").extract()}
-        for next_page in response.css("a::attr(href)").extract():
-            yield response.follow(next_page, self.parse)
+        item = {"url": response.url, "text": response.xpath("//text()").extract()}
+        self.items.append(item) 
+        yield item
 
 
 def save_texts_to_files(texts):
@@ -49,8 +61,15 @@ def save_texts_to_files(texts):
             f.write("\n".join(text_data["text"]))
 
 
-def index_to_elastic(texts, embeddings):
-    es = Elasticsearch()
+def index_to_elastic(texts, embeddings, es_host):
+    es = Elasticsearch(hosts=[es_host])
+
+    # Ensure the index exists before inserting documents
+    try:
+        if not es.indices.exists(index="text_embeddings"):
+            es.indices.create(index="text_embeddings")
+    except NotFoundError:
+        es.indices.create(index="text_embeddings")
 
     for idx, text_data in enumerate(texts):
         doc = {
@@ -58,7 +77,7 @@ def index_to_elastic(texts, embeddings):
             "text": "\n".join(text_data["text"]),
             "embedding": embeddings[idx].tolist(),
         }
-        es.index(index="text_embeddings", doc_type="_doc", body=doc)
+        es.index(index="text_embeddings",body=doc)
 
 
 def main():
@@ -70,23 +89,24 @@ def main():
     # Scrapy extraction
     process = CrawlerProcess()
     process.crawl(TextSpider)
-    texts = list(process.start())
+    process.start()
+
+    texts = TextSpider.items  # This will fetch the scraped items
+
 
     save_texts_to_files(texts)
 
     # Training model
     model = SentenceTransformer("paraphrase-distilroberta-base-v2")
+    # Prepare sentences for encoding
     sentences = ["\n".join(text_data["text"]) for text_data in texts]
-    train_dataset = SentencesDataset(sentences, model)
-    train_dataloader = DataLoader(train_dataset, shuffle=True, batch_size=32)
-    train_loss = losses.CosineSimilarityLoss(model)
-    model.fit(train_objectives=[(train_dataloader, train_loss)])
-    model.save("/models/embedding_model")
 
-    embeddings = model.encode(sentences, convert_to_tensor=True)
+    # Encode the sentences directly
+    embeddings = model.encode(sentences)
 
     # Index to Elasticsearch
-    index_to_elastic(texts, embeddings)
+    index_to_elastic(texts, embeddings, es_host)
+
 
 
 if __name__ == "__main__":
